a74542ebb8728f6e279ff02e4ace250079f40061
SanghooMoon <sanghoo.moon@gmail.com>
2020-11-17 15:11:56 +0900

    전체 조회 화면 만들기

--- src/main/java/com/jojodu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojodu/book/springboot/domain/posts/PostsRepository.java
@@ -1,11 +1,17 @@
 package com.jojodu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // myBatis 등에서 DAO로 불리는 접근자, JPA에선 Repository라고 부르며 인터페이스로 생성
 // JpaRepository<Entity 클래스, PK 타입> 으로 상속하면 기본 CRUD 메서드 자동 생성
 // 주의점은 Entity 클래스와 기본 Entity Repository는 함께 위치해야함. 둘은 밀접한관계
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
